a2559f4f7c47520b8758e11777421cfa10e748b8
Kanit Sharma <kanitshrm09@gmail.com>
2017-03-14 03:25:47 +0530

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ function fetchBookmarks(){
 
     bookmarksResults.innerHTML += '<div class="well">'+
                                   '<h3>'+name+
-                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a class="btn btn-default" target="_blank" href="http://'+url+'">Visit</a> ' +
                                   ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
                                   '</h3>'+
                                   '</div>';
